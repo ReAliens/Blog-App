e7a301a723c5e41e8ff8c1fb1ea3ebc143e0484f
Ahmed <ahmedreda152@gmail.com>
2022-03-25 01:31:13 +0200

    add tests for controller

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe "Posts controller", type: :request do
+  describe "GET /index" do
+    before :each do
+      get "/users/1/posts"
+    end
+    it "renders template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "correct status" do
+      expect(response.status).to eq(200)
+    end
+
+    it "body includes correct text" do
+      expect(response.body).to include("display list of posts")
+    end
+  end
+
+  describe "GET /show" do
+    before :each do
+      get "/users/1/posts/2"
+    end
+    it "renders template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "correct status" do
+      expect(response.status).to eq(200)
+    end
+
+    it "body includes correct text" do
+      expect(response.body).to include("display certain user posts ")
+    end
+  end
+end
