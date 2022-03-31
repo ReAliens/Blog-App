21a3f44bdc7e61f76ec7ed3d23ca7da1951c2552
Ahmed <ahmedreda152@gmail.com>
2022-04-01 01:32:54 +0200

    update migration to fix foreign keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  root "user#index"
+  root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
