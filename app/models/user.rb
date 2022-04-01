4e4c5fcd1924fd1775ed2acaa0e68ac1a72c50e3
Ahmed <ahmedreda152@gmail.com>
2022-04-02 00:28:06 +0200

    create controllers for comments and likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :likes, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: "author_id", dependent: :destroy
+  has_many :likes, foreign_key: "author_id", dependent: :destroy
+  has_many :comments, foreign_key: "author_id", dependent: :destroy
 
   def recent_posts
-    posts.order('created_at DESC').limit(3)
-  end
-
-  def current_user
-    USer.first
+    posts.order("created_at DESC").limit(3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments
+      resources :likes
+    end
   end
 end
