a60cee5d4bb32e365f03b0685f9e421e6ecd582e
Ahmed <ahmedreda152@gmail.com>
2022-04-02 02:14:32 +0200

    add forms for likes, comments, and create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,26 @@ class PostsController < ApplicationController
   end
 
   def show
-    @posts = Post.where(author_id: params[:user_id])
+    @post = Post.where(id: params[:id])[0]
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to "#{users_path}/#{current_user.id}" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:author_id, :title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :posts
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments
-      resources :likes
-    end
+    resources :posts, only: [:index, :show]
   end
+  get "/posts/new", to: "posts#new"
+  post "/posts/create", to: "posts#create"
+  post "/users/:user_id/posts/:id/like", to: "likes#create"
+  post "/users/:user_id/posts/:id/create_comment", to: "comments#create"
 end
