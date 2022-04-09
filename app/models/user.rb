df11fed24665b4e4bde2bcdd7f65b865a8475862
Ahmed <ahmedreda152@gmail.com>
2022-04-09 20:23:47 +0200

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ApplicationRecord
   attr_writer :login
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable, :timeoutable, :trackable
-  has_many :posts, foreign_key: "author_id", dependent: :destroy
-  has_many :comments, foreign_key: "author_id", dependent: :destroy
-  has_many :likes, foreign_key: "author_id", dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
   validates :name, presence: true
   validates_format_of :name, with: /^[a-zA-Z0-9_.]*$/, multiline: true
 
   def fetch_recent_posts
-    posts.order("created_at DESC").limit(3)
+    posts.order('created_at DESC').limit(3)
   end
 
   def admin?
-    role == "admin"
+    role == 'admin'
   end
 
   def login
@@ -24,7 +25,7 @@ class User < ApplicationRecord
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if (login = conditions.delete(:login))
-      where(conditions.to_h).where(["name = :value OR email = :value", { value: login }]).first
+      where(conditions.to_h).where(['name = :value OR email = :value', { value: login }]).first
     elsif conditions.key?(:name) || conditions.key?(:email)
       where(conditions.to_h).first
     end
