be4323e0220bd2f45627f55353d5376adfad67b7
Anselem <anselemodims@gmail.com>
2022-04-09 14:35:20 +0100

    Feat: make confirmation for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :likes, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments, foreign_key: 'author_id', dependent: :destroy
-
-  validates :name, presence: true, allow_blank: false
-  validates :posts_counter, numericality: { only_integer: true, greater_than: -1 }
-  validates :email, presence: true, :uniqueness => { :case_sensitive => false }
+  :recoverable, :rememberable, :validatable,
+  :confirmable, :lockable, :timeoutable, :trackable
+  has_many :posts, foreign_key: "author_id", dependent: :destroy
+  has_many :comments, foreign_key: "author_id", dependent: :destroy
+  has_many :likes, foreign_key: "author_id", dependent: :destroy
 
   def fetch_recent_posts
-    posts.order('created_at DESC').limit(3)
+  posts.order("created_at DESC").limit(3)
   end
 end
