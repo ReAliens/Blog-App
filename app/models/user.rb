f32d4ee4aabe4e481404ff72b1a527d8e8f0aa69
Ahmed <ahmedreda152@gmail.com>
2022-04-09 16:40:14 +0200

    fix migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments, foreign_key: 'author_id', dependent: :destroy
-  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: "author_id", dependent: :destroy
+  has_many :comments, foreign_key: "author_id", dependent: :destroy
+  has_many :likes, foreign_key: "author_id", dependent: :destroy
 
   def fetch_recent_posts
-    posts.order('created_at DESC').limit(3)
+    posts.order("created_at DESC").limit(3)
   end
 end
