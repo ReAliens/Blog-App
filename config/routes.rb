2f8a78acb2534f438ce2dfcd9e20b5f24c887ed3
Ahmed <ahmedreda152@gmail.com>
2022-04-09 16:18:06 +0200

    fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
   devise_scope :user do
     authenticated :user do
-      root :to => 'users#show', as: :authenticated_root
+      root :to => "users#show", as: :authenticated_root
     end
     unauthenticated :user do
-      root :to => 'devise/sessions#new', as: :unauthenticated_root
+      root :to => "devise/sessions#new", as: :unauthenticated_root
     end
   end
   resources :users, only: [:index, :show] do
